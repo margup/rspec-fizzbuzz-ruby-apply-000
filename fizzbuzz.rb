b98e9f4add04d8a9188dafc6e4f76e635dd30b9b
bright-lifecycle-9875 <mairgup@icloud.com>
2020-08-30 22:48:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,10 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz()
+def fizzbuzz(x)
+  if x % 3 == Integer
+    "Fizz"
+    elsif x % 5 == Integer
+    "Buzz"
   
 end
